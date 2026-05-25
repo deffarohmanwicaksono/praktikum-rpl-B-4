eff02971ae7df1749615d5fec2251b12059a376c
Nurul Janati Ramadhani <nurj.ramadhani@gmail.com>
2026-05-26 00:12:59 +0700

    feat: add login user model migration and seeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -17,9 +17,41 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        // Admin
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin SeMart',
+            'email' => 'admin@student.uns.ac.id',
+            'role' => 'admin',
+            'password' => 'password_admin',
+        ]);
+
+        // User
+        User::factory()->create([
+            'name' => 'Nurul Janati',
+            'email' => 'nurul@student.uns.ac.id',
+            'role' => 'buyer',
+            'password' => 'password123',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Syifa Qurrota',
+            'email' => 'syifa@student.uns.ac.id',
+            'role' => 'buyer',
+            'password' => 'password123',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Deffa Rohman',
+            'email' => 'deffa@student.uns.ac.id',
+            'role' => 'seller',
+            'password' => 'password123',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Kemal Amangylyjow',
+            'email' => 'kemal@student.uns.ac.id',
+            'role' => 'seller',
+            'password' => 'password123',
         ]);
     }
 }
