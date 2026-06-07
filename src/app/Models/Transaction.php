47ddba43eb9b32e2a419596a2284604b315bd6a7
deffarohmanwicaksono <deffaeight@gmail.com>
2026-06-07 16:10:20 +0700

    fix: define transaction relationship on Review model and review relationship on Transaction model

--- src/app/Models/Transaction.php
+++ src/app/Models/Transaction.php
@@ -43,4 +43,9 @@ class Transaction extends Model
     {
         return $this->belongsTo(PurchaseLink::class);
     }
+
+    public function review()
+    {
+        return $this->hasOne(Review::class);
+    }
 }
