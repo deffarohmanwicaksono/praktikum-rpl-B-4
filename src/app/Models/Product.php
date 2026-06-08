bfdec74fc5f3a9e62744e5895d96c14e480f3b79
Nurul Janati Ramadhani <nurj.ramadhani@gmail.com>
2026-06-08 14:53:58 +0700

    feat: make ProductFactory and synchronize timestamp

--- src/app/Models/Product.php
+++ src/app/Models/Product.php
@@ -3,15 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'user_id',
         'category_id',
         'name',
         'description',
         'price',
+        'stock',
         'status'
     ];
 
